189e14f553808c664b1a2f616fb7171d46284ca5
aoleagagoitia <aoleagagoitia@fp.uoc.edu>
2019-05-29 17:50:13 +0200

    Update parameters.php

--- config/parameters.php
+++ config/parameters.php
@@ -1,7 +1,7 @@
 <?php
 //Aquí defino diversos parámetros del proyecto, llamadas imagenes en las vistas...
-define("base_url", "http://localhost/P9a/");//defino base_url para usarlo más adelante
+define("base_url", "http://localhost/p9/");//defino base_url para usarlo más adelante
 define("controller_default", "ProductoController");//defino el controlador por defecto
 define("action_default", "index");//defino el método por defecto
 
-//cambio servidor linea 3: http://local/P9a/
+//cambio servidor linea 3: http://local/p9/
